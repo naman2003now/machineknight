eb16621d3cd06a9594b4610ce9ff32070254706d
Naman Agrawal <a6.namanagrawal@gmail.com>
2022-09-04 18:39:34 +0530

    CORS

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 app = Flask(__name__)  
+CORS(app)
  
 @app.route('/')  
 def home():  
     print(request.json)
+
     rent = 1000
     return str(rent)  
   
